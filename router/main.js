085b0d82aca5a78c87c519d57d6228779c732f55
Ajinkya Kshirsagar <ajinkya.tesla@gmail.com>
2016-06-11 19:34:37 -0700

    prettify

--- router/main.js
+++ router/main.js
@@ -1,33 +1,30 @@
+module.exports = function(app) {
+    var loginAuth = require('../middleware/loginAuth');
+    var bodyParser = require('body-parser');
+    var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-module.exports = function(app)
-{
-	var loginAuth = require('../middleware/loginAuth');
-	var bodyParser = require('body-parser');
-	var urlencodedParser = bodyParser.urlencoded({ extended: false })
+    var userDB = ['admin@minesweepers.co', 'a@b.c'];
+    var password = 'admin123';
 
-	var userDB = ['admin@minesweepers.co', 'a@b.c'];
-	var password = 'sukhadalund1';
-
-   	app.get('/', loginAuth.requireCookies, function(req,res){
+    app.get('/', loginAuth.requireCookies, function(req, res) {
         console.log('cookies', req.cookies);
         res.render('index.html');
     });
 
-    app.get('/login', loginAuth.alreadyLoggedIn, function(req,res){
-    	console.log('cookies', req.cookies);
+    app.get('/login', loginAuth.alreadyLoggedIn, function(req, res) {
+        console.log('cookies', req.cookies);
         res.render('login.html');
     });
 
-    app.post('/login', urlencodedParser, function(req,res){
+    app.post('/login', urlencodedParser, function(req, res) {
 
-        if(  req.body.username && userDB.indexOf(req.body.username) > -1  
-        	&& req.body.password && req.body.password === password ) {
-        	console.log('give cookies');
-        	res.cookie('cookiename', 'cookievalue', { maxAge: 900000, httpOnly: true });
-        	res.writeHead(302, {
-  				'Location': '/'
-			});
-			res.end();
+        if (req.body.username && userDB.indexOf(req.body.username) > -1 && req.body.password && req.body.password === password) {
+            console.log('give cookies');
+            res.cookie('cookiename', 'cookievalue', { maxAge: 900000, httpOnly: true });
+            res.writeHead(302, {
+                'Location': '/'
+            });
+            res.end();
         }
         res.status(403);
         res.end('NOT_AUTHORIZED');
